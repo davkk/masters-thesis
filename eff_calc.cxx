94b39336282f464692049eaca892eab33576c3d7
Dawid Karpiński <dawid.karp@int.pl>
2025-03-20 17:17:31 +0100

    update 1742487451

--- eff_calc.cxx
+++ eff_calc.cxx
@@ -8,10 +8,9 @@
 #include <cassert>
 #include <filesystem>
 #include <format>
-#include <iostream>
 #include <string>
 
-using namespace std::string_literals;
+namespace fs = std::filesystem;
 
 template <typename H>
 H* get_histogram(TFile* file, const std::string& name) {
@@ -23,14 +22,12 @@ H* clone_histogram(H* hist, const std::string& name) {
   return dynamic_cast<H*>(hist->Clone(name.c_str()));
 }
 
-void eff_calc(const std::string& results_path_str, const std::string& task_name) {
+void eff_calc(const fs::path& results_path, const fs::path& reco_path, const fs::path& truth_path) {
   auto is_alien {false};
-  if (results_path_str.starts_with("alien://")) {
+  if (results_path.string().starts_with("alien://")) {
     TGrid::Connect("alien://");
     is_alien = true;
   }
-
-  const auto results_path {std::filesystem::path(results_path_str)};
 
   auto* result_file = TFile::Open(results_path.c_str());
   assert(result_file != nullptr && !result_file->IsZombie());
@@ -42,112 +39,82 @@ void eff_calc(const std::string& results_path_str, const std::string& task_name)
   auto* output_file {TFile::Open(output_path.c_str(), "RECREATE")};
   assert(output_file != nullptr && !output_file->IsZombie());
 
-  auto part_names {std::vector {"one", "two"}};
+  // Retrieve reco and truth histograms
+  const auto reco_true_path {fs::path(std::format("{}_MC", reco_path.c_str()))};
+  auto* hist_reco_true {get_histogram<TH1F>(result_file, reco_true_path / "hPt")};
+  assert(hist_reco_true);
 
-  // Process Each Particle
-  for (size_t idx = 0; idx < 2; ++idx) {
-    std::cout << "Processing " << part_names[idx] << '\n';
+  auto* hist_truth {get_histogram<TH1F>(result_file, truth_path / "hPt")};
+  assert(hist_truth);
 
-    // Retrieve reco and truth histograms
-    auto* hist_reco_mc {get_histogram<TH1F>(result_file, std::format("{}/Tracks_{}_MC/hPt", task_name, part_names[idx]))
-    };
-    auto* hist_truth {
-        get_histogram<TH1F>(result_file, std::format("{}/MCTruthTracks_{}/hPt", task_name, part_names[idx]))
-    };
+  assert(hist_reco_true->GetXaxis()->GetXmin() == hist_truth->GetXaxis()->GetXmin());
+  assert(hist_reco_true->GetXaxis()->GetBinWidth(1) == hist_truth->GetXaxis()->GetBinWidth(1));
 
-    if (hist_reco_mc == nullptr || hist_truth == nullptr) {
-      std::cout << "Skipping " << part_names[idx] << '\n';
-      continue;
-    }
+  // Calculate Efficiency
+  auto* hist_eff {clone_histogram(hist_reco_true, "hEfficiency")};
+  for (auto bin_idx {1}; bin_idx <= hist_eff->GetNbinsX(); ++bin_idx) {
+    auto reco_value {hist_reco_true->GetBinContent(bin_idx)};
+    auto truth_value {hist_truth->GetBinContent(bin_idx)};
 
-    assert(hist_reco_mc);
-    assert(hist_truth);
-
-    assert(hist_reco_mc->GetXaxis()->GetXmin() == hist_truth->GetXaxis()->GetXmin());
-    assert(hist_reco_mc->GetXaxis()->GetBinWidth(1) == hist_truth->GetXaxis()->GetBinWidth(1));
-
-    // Calculate Efficiency
-    auto* hist_eff {clone_histogram(hist_reco_mc, std::format("hEfficiency_part{}", idx + 1))};
-    for (auto bin_idx {1}; bin_idx <= hist_eff->GetNbinsX(); ++bin_idx) {
-      auto reco_value {hist_reco_mc->GetBinContent(bin_idx)};
-      auto truth_value {hist_truth->GetBinContent(bin_idx)};
-
-      auto eff {(truth_value > 0) ? reco_value / truth_value : 0};
-      hist_eff->SetBinContent(bin_idx, eff);
-    }
-    output_file->WriteTObject(hist_eff);
-
-    // Calculate Contamination
-    // daughters + material
-    auto* hist_reco {get_histogram<TH1F>(result_file, std::format("{}/Tracks_{}/hPt", task_name, part_names[idx]))};
-    auto* hist_secondary {
-        get_histogram<TH2F>(result_file, std::format("{}/Tracks_{}_MC/hDCAxy_Daughter", task_name, part_names[idx]))
-    };
-    auto* hist_material {
-        get_histogram<TH2F>(result_file, std::format("{}/Tracks_{}_MC/hDCAxy_Material", task_name, part_names[idx]))
-    };
-
-    assert(hist_reco);
-    assert(hist_secondary);
-
-    assert(hist_secondary->GetXaxis()->GetBinWidth(1) == hist_reco->GetXaxis()->GetBinWidth(1));
-    assert(hist_secondary->GetXaxis()->GetBinLowEdge(1) == hist_reco->GetXaxis()->GetBinLowEdge(1));
-
-    auto* hist_secondary_x {hist_secondary->ProjectionX(std::format("hSecondaryContamination_part{}", idx + 1).c_str())
-    };
-    hist_secondary_x->Add(hist_material->ProjectionX());
-
-    for (int bin_idx = 1; bin_idx <= hist_secondary_x->GetNbinsX(); ++bin_idx) {
-      auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
-      auto reco_value {hist_reco->GetBinContent(bin_idx)};
-      hist_secondary_x->SetBinContent(bin_idx, reco_value > 0 ? cont_value / reco_value : 0);
-    }
-
-    output_file->WriteTObject(hist_secondary_x);
-
-    auto* hist_fake {
-        get_histogram<TH2F>(result_file, std::format("{}/Tracks_{}_MC/hDCAxy_Fake", task_name, part_names[idx]))
-    };
-
-    assert(hist_fake);
-
-    assert(hist_fake->GetXaxis()->GetBinWidth(1) == hist_reco->GetXaxis()->GetBinWidth(1));
-    assert(hist_fake->GetXaxis()->GetBinLowEdge(1) == hist_reco->GetXaxis()->GetBinLowEdge(1));
-
-    auto* hist_fake_x {hist_secondary->ProjectionX(std::format("hFake_part{}", idx + 1).c_str())};
-
-    for (int bin_idx = 1; bin_idx <= hist_fake_x->GetNbinsX(); ++bin_idx) {
-      auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
-      auto reco_value {hist_reco->GetBinContent(bin_idx)};
-      hist_fake_x->SetBinContent(bin_idx, reco_value > 0 ? cont_value / reco_value : 0);
-    }
-
-    output_file->WriteTObject(hist_fake_x);
-
-    assert(hist_secondary_x->GetXaxis()->GetBinWidth(1) == hist_eff->GetXaxis()->GetBinWidth(1));
-    assert(hist_secondary_x->GetXaxis()->GetBinLowEdge(1) == hist_eff->GetXaxis()->GetBinLowEdge(1));
-
-    // Calculate Weights
-    auto* weights {clone_histogram(hist_secondary_x, std::format("hWeights_part{}", idx + 1))};
-    for (int bin_idx = 1; bin_idx <= weights->GetNbinsX(); ++bin_idx) {
-      auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
-      auto eff_value {hist_eff->GetBinContent(bin_idx)};
-
-      auto weight {(eff_value > 0) ? (1 - cont_value) / eff_value : 1};
-      weights->SetBinContent(bin_idx, weight);
-    }
-    output_file->WriteTObject(weights);
+    auto eff {(truth_value > 0) ? reco_value / truth_value : 0};
+    hist_eff->SetBinContent(bin_idx, eff);
   }
+  output_file->WriteTObject(hist_eff);
+
+  // Calculate Contamination
+  // daughters + material
+  auto* hist_reco {get_histogram<TH1F>(result_file, reco_path / "hPt")};
+  auto* hist_secondary {get_histogram<TH2F>(result_file, reco_true_path / "hDCAxy_Daughter")};
+  auto* hist_material {get_histogram<TH2F>(result_file, reco_true_path / "hDCAxy_Material")};
+
+  assert(hist_reco);
+  assert(hist_secondary);
+
+  assert(hist_secondary->GetXaxis()->GetBinWidth(1) == hist_reco->GetXaxis()->GetBinWidth(1));
+  assert(hist_secondary->GetXaxis()->GetBinLowEdge(1) == hist_reco->GetXaxis()->GetBinLowEdge(1));
+
+  auto* hist_secondary_x {hist_secondary->ProjectionX("hSecondary")};
+  hist_secondary_x->Add(hist_material->ProjectionX());
+  for (int bin_idx = 1; bin_idx <= hist_secondary_x->GetNbinsX(); ++bin_idx) {
+    auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
+    auto reco_value {hist_reco->GetBinContent(bin_idx)};
+    hist_secondary_x->SetBinContent(bin_idx, reco_value > 0 ? cont_value / reco_value : 0);
+  }
+  output_file->WriteTObject(hist_secondary_x);
+
+  auto* hist_fake {get_histogram<TH2F>(result_file, reco_true_path / "hDCAxy_Fake")};
+  assert(hist_fake);
+  assert(hist_fake->GetXaxis()->GetBinWidth(1) == hist_reco->GetXaxis()->GetBinWidth(1));
+  assert(hist_fake->GetXaxis()->GetBinLowEdge(1) == hist_reco->GetXaxis()->GetBinLowEdge(1));
+
+  auto* hist_fake_x {hist_secondary->ProjectionX("hFake")};
+  for (int bin_idx = 1; bin_idx <= hist_fake_x->GetNbinsX(); ++bin_idx) {
+    auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
+    auto reco_value {hist_reco->GetBinContent(bin_idx)};
+    hist_fake_x->SetBinContent(bin_idx, reco_value > 0 ? cont_value / reco_value : 0);
+  }
+  output_file->WriteTObject(hist_fake_x);
+
+  assert(hist_secondary_x->GetXaxis()->GetBinWidth(1) == hist_eff->GetXaxis()->GetBinWidth(1));
+  assert(hist_secondary_x->GetXaxis()->GetBinLowEdge(1) == hist_eff->GetXaxis()->GetBinLowEdge(1));
+
+  // Calculate Weights
+  auto* weights {clone_histogram(hist_secondary_x, "hWeights")};
+  for (int bin_idx = 1; bin_idx <= weights->GetNbinsX(); ++bin_idx) {
+    auto cont_value {hist_secondary_x->GetBinContent(bin_idx)};
+    auto eff_value {hist_eff->GetBinContent(bin_idx)};
+
+    auto weight {(eff_value > 0) ? (1 - cont_value) / eff_value : 1};
+    weights->SetBinContent(bin_idx, weight);
+  }
+  output_file->WriteTObject(weights);
 
   output_file->Close();
   result_file->Close();
-
-  delete output_file;
-  delete result_file;
 }
 
 int main(int argc, char** argv) {
-  assert(argc == 3);
-  eff_calc(std::string(argv[1]), std::string(argv[2]));
+  assert(argc == 4);
+  eff_calc(argv[1], argv[2], argv[3]);
   return 0;
 }
--- upload.sh
+++ upload.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
 
-# for i in `seq 1 2`; do
-#     echo "Uploading for particle $i"
-#     o2-ccdb-upload \
-#         --host http://alice-ccdb.cern.ch \
-#         --path Users/d/dkarpins/Correction \
-#         --file ./data/LHC23d1k/pi-api/EfficiencyCorrection.root \
-#         --key hWeights_part$i \
-#         --meta "dataset=LHC23d1k;particle=pi"
-# done
-
 o2-ccdb-upload \
     --host http://alice-ccdb.cern.ch \
     --path Users/d/dkarpins/Correction \
     --file ./data/LHC23d1k/pi-api/EfficiencyCorrection.root \
-    --key hWeights_part1 \
-    --meta "dataset=LHC23d1k;particle=pi;trainRun=373670"
-
-o2-ccdb-upload \
-    --host http://alice-ccdb.cern.ch \
-    --path Users/d/dkarpins/Correction \
-    --file ./data/LHC23d1k/pi-api/EfficiencyCorrection.root \
-    --key hWeights_part2 \
+    --key hWeights \
     --meta "dataset=LHC23d1k;particle=api;trainRun=373670"
