1d77ca6301fcd14a2dd28cd0417e262120327219
Dawid Karpiński <dawid.karp@int.pl>
2025-04-09 12:47:52 +0200

    update 1744195672

--- eff_corr.sh
+++ eff_corr.sh
@@ -7,10 +7,10 @@ name=`basename ${0%.*}`
 g++ `root-config --libs --glibs --cflags` --std=c++20 -O3 ${name}.cxx -o ${name}.out
 
 ./${name}.out \
-    ./data/LHC24f3c/p-p/382926.root \
+    ./data/LHC23d1k/p-p/385462.root \
     femto-universe-pair-task-track-track-extended_p-p_nocor/Tracks_one \
     femto-universe-pair-task-track-track-extended_p-p_nocor/MCTruthTracks_one
-# mv ./data/LHC24f3c/p-p/EfficiencyCorrection.root ./data/LHC24f3c/p-p/EfficiencyCorrection1-new.root
+mv ./data/LHC23d1k/p-p/EfficiencyCorrection.root ./data/LHC23d1k/p-p/EfficiencyCorrection-new.root
 
 # ./${name}.out \
 #     ./data/LHC23d1k/pi-api/373670.root \
