f8617df85bdada0d5cad58d4d585dfc59a901702
Dawid Karpiński <dawid.karp@int.pl>
2025-05-07 18:29:38 +0200

    update 1746635378

--- upload.sh
+++ upload.sh
@@ -2,9 +2,18 @@
 
 set -euo pipefail
 
+train_run=388604
+
+# o2-ccdb-upload \
+#     --host http://alice-ccdb.cern.ch \
+#     --path Users/d/dkarpins/Correction \
+#     --file ./data/LHC24f3c\(nocont\)/pi-api/${train_run}-1-effcor.root \
+#     --key hWeights \
+#     --meta "dataset=LHC24f3c;particle=pi;trainRun=${train_run};nocont=1"
+
 o2-ccdb-upload \
     --host http://alice-ccdb.cern.ch \
     --path Users/d/dkarpins/Correction \
-    --file ./data/LHC24f3c/pi-api/387037-effcor-1744287852397.root \
+    --file ./data/LHC24f3c/p-p/388604.root \
     --key hWeights \
-    --meta "dataset=LHC24f3c;particle=api;trainRun=387037"
+    --meta "dataset=LHC24f3c;particle=p;trainRun=388604"
